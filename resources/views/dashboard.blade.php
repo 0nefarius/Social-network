3313925e4756ff6b6f5e3535233f28784a501f98
0nefarius <wojtek.jandura@gmail.com>
2020-02-24 15:57:01 +0100

    Dashboard View

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,2 +1,53 @@
-<h1>The Dashboard</h1></h>
+@extends('layouts.master')
 
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What do you have to say?</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-3-offser">
+            <header><h3><br />What other people say...</h3></header>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="info">
+                    Posted by Wojtek on 12 Feb 2020
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a> |
+                </div>
+            </article>
+            <article class="post">
+                <p><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="info">
+                    Posted by Wojtek on 12 Feb 2020
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+
+            </article>
+        </div>
+    </section>
+@endsection
