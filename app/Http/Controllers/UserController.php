e0bf2188ef8bc10b577492acca9f39d2bb77b6fb
0nefarius <wojtek.jandura@gmail.com>
2020-02-24 14:06:04 +0100

    Logging users in

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,11 +1,18 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class UserController extends Controller
 {
+    public function getDashboard()
+    {
+        return view('dashboard');
+    }
+
     public function postSignUp(Request $request)
     {
         $email = $request['email'];
@@ -19,11 +26,15 @@ class UserController extends Controller
 
         $user->save();
 
-        return redirect()->back();
+        Auth::login($user);
+        return redirect()->route('dashboard');
     }
 
-    public function postSignIn()
+    public function postSignIn(Request $request)
     {
-
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+            return redirect()->route('dashboard');
+        }
+        return redirect()->back();
     }
 }
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,2 @@
+<h1>The Dashboard</h1></h>
+
