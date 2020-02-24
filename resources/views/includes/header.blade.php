86279b51264d679426b739655f9dab446a105899
0nefarius <wojtek.jandura@gmail.com>
2020-02-24 17:38:48 +0100

    Css style upload

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,19 +1,19 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark" style="margin-bottom: 20px;">
+        <a class="navbar-brand" style="color: white;" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse" id="navbarNavDropdown" style="color: white;">
             <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <li class="nav-item active" >
+                    <a class="nav-link" style="color: white;" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" style="color: white;" href="#">Features</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" style="color: white;" href="#">Pricing</a>
                 </li>
             </ul>
         </div>
