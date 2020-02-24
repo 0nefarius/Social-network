8d55af3c627d95187b6b93945b02e91e52588807
0nefarius <wojtek.jandura@gmail.com>
2020-02-24 15:11:40 +0100

    Middleware and route protection

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::get('/', function () {
         return view('welcome');
-    });
+    })->name('home');
 
     Route::post('/signup', [
         'uses' => 'UserController@postSignUp',
@@ -31,7 +31,8 @@ Route::group(['middleware' => ['web']], function () {
 
     Route::get('/dashboard', [
         'uses' => 'UserController@getDashboard',
-        'as' => 'dashboard'
+        'as' => 'dashboard',
+        'middleware' => 'auth'
     ]);
 
 
