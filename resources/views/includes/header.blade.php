07cd9e603b6a743814d1b0180d396e7357d72363
0nefarius <wojtek.jandura@gmail.com>
2020-04-06 16:53:40 +0200

    User account, image uploading

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,12 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top" style="margin-bottom: 30px;">
-    <a class="navbar-brand" href="#">m8s.com</a>
+    <a class="navbar-brand" href="{{ route('dashboard') }}">m8s.com</a>
     <ul class="navbar-nav navbar-right">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
                 Dropdown link
             </a>
             <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{ route('account') }}">Account</a>
                 <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
             </div>
         </li>
